b2cbd71d6de905cfd6b1673dbbff2f2d6c8b36a0
cebial <biglazypanda@gmail.com>
2016-03-12 09:48:05 +0100

    Second solution to largest-series-product

--- largest-series-product/largest-series-product.js
+++ largest-series-product/largest-series-product.js
@@ -1,20 +1,17 @@
 module.exports = function (num) {
-    return {
-        get digits () { return num.split('').map(Number); },
-        slices: function (n) {
-            return num.split('').reduce(function (a, _, i) {
-                return num[i + n - 1] ? a.concat([num.substr(i, n).split('').map(Number)]) : a;
-            }, []);
-        },
-        largestProduct: function (n) {
-            if (n > num.length) throw Error('Slice size is too big.');
-            if (!num || !n) return 1;
+    var digits = num.split('').map(Number);
+    var slices = function (n) {
+        if (n > num.length) throw Error('Slice size is too big.');
 
-            return this.slices(n).reduce(function (max, tuple) {
-                return Math.max(max, tuple.reduce(function (x, y) {
-                    return x * y;
-                }));
-            }, 0);
-        }
-    }
+        return digits.reduce(function (a, _, i) {
+            return num[i + n - 1] ? a.concat([digits.slice(i, i + n)]) : a;
+        }, []);
+    };
+    var largestProduct = function (n) {
+        return !n ? 1 : slices(n).reduce(function (max, tuple) {
+            return Math.max(max, eval(tuple.join('*')));
+        }, 0);
+    };
+
+    return { digits, slices, largestProduct };
 };
